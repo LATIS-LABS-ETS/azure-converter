5211e6efcad3a9f99bb275f427649a87471050d8
#0K Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2023-08-17 15:59:48 +0000

    Updates
    - Shell script automated based on mkv files in captures folder

---
+++ batch_converter.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd build
+search_dir=../captures/
+for videopath in "$search_dir"/*
+do
+    filename="$(basename ${videopath%.*})"
+    rgbdir=${filename}/RGB
+    depthdir=${filename}/DEPTH
+    command="./AzureConverter -vpath ${videopath} -format 2 --depthdir ${depthdir} --rgbdir ${rgbdir}"
+    tarcommand="tar -cvzf ./${filename}.tar.gz ./${filename}"
+    echo "$filename"
+    echo "$rgbdir"
+    echo "$depthdir"
+    $command
+    $tarcommand
+    rm -Rf ./${filename}
+done
+
+
+# ./AzureConverter -vpath ../captures/kev.mkv -format 2 --depthdir ./Kevin-RGB2D/DEPTH --rgbdir ./Kevin-RGB2D/RGB;
+# ./AzureConverter -vpath ../captures/kev3.mkv -format 2 --depthdir ./Kevin3-RGB2D/DEPTH --rgbdir ./Kevin3-RGB2D/RGB;
+# ./AzureConverter -vpath ../captures/baby_mannequin.mkv -format 2 --depthdir ./Baby-Mannequin-RGB2D/DEPTH --rgbdir ./Baby-Mannequin-RGB2D/RGB;
+# ./AzureConverter -vpath ../captures/kid1_mannequin.mkv -format 2 --depthdir ./Kid1-Mannequin-RGB2D/DEPTH --rgbdir ./Kid1-Mannequin-RGB2D/RGB;
+# ./AzureConverter -vpath ../captures/kid2_mannequin.mkv -format 2 --depthdir ./Kid2-Mannequin-RGB2D/DEPTH --rgbdir ./Kid2-Mannequin-RGB2D/RGB;
+# ./AzureConverter -vpath ../captures/adolescent_mannequin.mkv -format 2 --depthdir ./Adolescent-Mannequin-RGB2D/DEPTH --rgbdir ./Adolescent-Mannequin-RGB2D/RGB; 
+# ./AzureConverter -vpath ../captures/Kevin-10-49-57-12.mkv -format 2 --depthdir ./Kevin-10-49-57-12/DEPTH --rgbdir ./Kevin-10-49-57-12/RGB;
+# ./AzureConverter -vpath ../captures/Kevin-10-53-56-80.mkv -format 2 --depthdir ./Kevin-10-53-56-80/DEPTH --rgbdir ./Kevin-10-53-56-80/RGB;
+# ./AzureConverter -vpath ../captures/Kevin-10-49-57-12.mkv -format 2 --depthdir ./Kevin-10-49-57-12/DEPTH --rgbdir ./Kevin-10-49-57-12/RGB;
+# ./AzureConverter -vpath ../captures/Kevin-10-49-57-12.mkv -format 2 --depthdir ./Kevin-10-49-57-12/DEPTH --rgbdir ./Kevin-10-49-57-12/RGB;
